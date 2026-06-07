98205ac98e12f72681b19fdeca63de37681a3598
prithu000 <prithumaheshwari@gmail.com>
2026-06-08 00:46:02 +0530

    Configure dj-database-url for production deployment

--- camcap/settings.py
+++ camcap/settings.py
@@ -104,11 +104,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'camcap.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 LANGUAGE_CODE = 'en-us'
